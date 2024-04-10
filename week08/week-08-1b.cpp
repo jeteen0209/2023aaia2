6ce0f920a9b8663676cf9846f17e1ca1bec2f59d
jeteen0209 <jeteen501@gmail.com>
2024-04-10 09:57:24 +0800

    week08

---
+++ week08/week-08-1b.cpp
@@ -0,0 +1,12 @@
+///week08-1.cpp
+#include <stdio.h>
+int main()
+{
+
+    for(int i=1; i<10; i++){
+        for(int j=1; j<9; j++){
+            printf("*");
+        }
+        printf("i:%d\n", i);
+    }
+}
